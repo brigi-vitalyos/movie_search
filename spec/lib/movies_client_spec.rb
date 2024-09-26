d098be18796d7bd08e5377c0493cbdb9a1c4878a
Brigitta Vitalyos <brigi.vitalyos@gmail.com>
2024-09-26 14:29:04 +0200

    Introduce empty MoviesClient

---
+++ spec/lib/movies_client_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../../lib/movies_client.rb'
+
+RSpec.describe MoviesClient do
+  subject(:client) { described_class.new }
+
+  describe '#search' do
+
+    it 'happy_path' do
+    end
+  end
+end
