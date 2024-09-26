c7113c9a8f09ee080af4fb9a676f763f36be96c5
Brigitta Vitalyos <brigi.vitalyos@gmail.com>
2024-09-26 17:18:03 +0200

    Store cache hits for searches

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,14 +5,16 @@ class MoviesController < ApplicationController
   def search
     movies_repo = MoviesRepository.new
 
-    cached_result = movies_repo.fetch query_string, page_number
-    if cached_result.nil? || cached_result.empty?
-      response = MoviesClient.new.search(query_string)
-      movies_repo.store query_string, page_number, response
-      @movies = process_movies(response['results'])
+    result = movies_repo.fetch query_string, page_number
+    if result.nil? || result.empty?
+      result = MoviesClient.new.search(query_string)
+      movies_repo.store query_string, page_number, result
+      CacheHit.create! query_string: query_string, count: 0
     else
-      @movies = process_movies(cached_result['results'])
+      @cache_hit_count = cache_hit_for(query_string).count + 1
+      cache_hit_for(query_string).update count: @cache_hit_count
     end
+    @movies = process_movies(result['results'])
     render :index
   rescue
     redirect_to '/500'
@@ -27,6 +29,9 @@ class MoviesController < ApplicationController
 
   private
 
+  def cache_hit_for(query_string)
+    @cache_hit ||= CacheHit.where(query_string: query_string).where('created_at > ?', 2.minutes.ago.to_s).first
+  end
   def query_string
     params[:query_string]
   end
