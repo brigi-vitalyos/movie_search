c051a5a712d72e6ab7a40ac5fae31160b509334f
Brigitta Vitalyos <brigi.vitalyos@gmail.com>
2024-09-28 16:49:15 +0200

    Restructure js files

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -2,6 +2,7 @@
 // import "@hotwired/turbo-rails"
 // import "app/assets/javascript/controllers"
 //
-// = require jquery3
+// = require ../../../../../.local/share/mise/installs/ruby/3.3.5/lib/ruby/gems/3.3.0/gems/jquery-rails-4.6.0/vendor/assets/javascripts/jquery3
 // = require popper
 // = require bootstrap
+
