44184753ccdf7e9633c4c60ecc500048652b21f0
Brigitta Vitalyos <brigi.vitalyos@gmail.com>
2024-09-29 14:19:37 +0200

    Add missing tests for Movies model

--- spec/app/models/movie_spec.rb
+++ spec/app/models/movie_spec.rb
@@ -1,4 +1,46 @@
-# require 'rails_helper'
-#
-# RSpec.describe Movie, type: :model do
-# end
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  subject(:movie) { described_class.new movie_config }
+
+  let(:movie_config) do
+    {'adult'=>false,
+     'backdrop_path'=>'/xyz.jpg',
+     'genre_ids'=>[18, 35],
+     'id'=>293456,
+     'original_language'=>'hu',
+     'original_title'=>'Something original',
+     'overview'=> 'Some description.',
+     'popularity'=>1.521,
+     'poster_path'=>'/xzy.jpg',
+     'release_date'=>'2014-07-10',
+     'title'=>'Something',
+     'video'=>false,
+     'vote_average'=>7.1,
+     'vote_count'=>25}
+  end
+
+  describe '#title' do
+    subject(:title) { movie.title }
+
+    it 'returns the movie title' do
+      expect(title).to eq movie_config['title']
+    end
+  end
+
+  describe '#description' do
+    subject(:description) { movie.description }
+
+    it 'returns the movie description' do
+      expect(description).to eq movie_config['overview']
+    end
+  end
+
+  describe '#poster_path' do
+    subject(:poster_path) { movie.poster_path }
+
+    it 'returns the movie poster_path' do
+      expect(poster_path).to eq movie_config['poster_path']
+    end
+  end
+end
